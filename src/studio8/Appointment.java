1ab331267aa0993226807e53bdb659c4d2d05456
samueltoy <samueltoy@localhost>
2022-11-16 17:03:23 -0600

    Done

--- src/studio8/Appointment.java
+++ src/studio8/Appointment.java
@@ -7,13 +7,13 @@ import java.util.Objects;
 public class Appointment {
 	private Date date;
 	private Time time;
-	
+
 	public Appointment(Date date, Time time) {
-	    this.date = date;
-	    this.time = time;
-	    
+		this.date = date;
+		this.time = time;
+
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(date, time);
@@ -31,34 +31,29 @@ public class Appointment {
 		return Objects.equals(date, other.date) && Objects.equals(time, other.time);
 	}
 
-	
-	
 	@Override
 	public String toString() {
-		return "Appointment on " + date + " at " +  time;
+		return "Appointment on " + date + " at " + time;
 	}
 
 	public static void main(String[] args) {
-		Date a = new Date (10,10,2010);
-		Date b = new Date (10,10,2010);
-		Date c = new Date (10,10,2010);
-		
-		Time d = new Time(4,20);
-	    Time e = new Time(4,21);
-	    Time f = new Time(4,22);
-		
-		Appointment g = new Appointment(a,d);
-		Appointment h = new Appointment(b,e);
-		Appointment i = new Appointment(c,f);
-		
-		 HashSet<Appointment> set = new HashSet<Appointment>();
-		    set.add(g);
-		    set.add(h);
-		    set.add(i);
-		    System.out.println(set);
-		    }
-		
+		Date a = new Date(10, 10, 2010);
+		Date b = new Date(10, 10, 2010);
+		Date c = new Date(10, 10, 2010);
 
+		Time d = new Time(4, 20);
+		Time e = new Time(4, 21);
+		Time f = new Time(4, 22);
+
+		Appointment g = new Appointment(a, d);
+		Appointment h = new Appointment(b, e);
+		Appointment i = new Appointment(c, f);
+
+		HashSet<Appointment> set = new HashSet<Appointment>();
+		set.add(g);
+		set.add(h);
+		set.add(i);
+		System.out.println(set);
 	}
 
-
+}
